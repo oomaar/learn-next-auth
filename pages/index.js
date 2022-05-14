df838911ee0d59719f4f446dd8271cc78421156c
oomaar <omaar_5@hotmail.com>
2022-05-14 19:05:58 +0200

    display user info

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,23 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home = () => {
   const { data: session } = useSession();
-  console.log("🚀 ~ file: index.js ~ line 6 ~ Home ~ session", session);
+  const user = session.user;
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-slate-50">
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-slate-50">
+      <div className="flex flex-col items-center mb-10">
+        <img
+          className="border rounded-full w-40"
+          src={user.image}
+          alt="User Image"
+        />
+        <p className="text-2xl font-semibold">
+          Loged in as: <span className="text-green-700">{user.name}</span>
+        </p>
+        <p className="text-2xl font-semibold">
+          Email: <span className="text-green-700">{user.email}</span>
+        </p>
+      </div>
       <div
         className="flex cursor-pointer w-64 h-auto px-4 py-2 border rounded-md border-gray-300 items-center justify-center"
         onClick={signIn}
