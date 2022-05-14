7abea78372d628f44bcdfd9d957d931bf0627d2c
oomaar <omaar_5@hotmail.com>
2022-05-14 17:33:20 +0200

    handle the firebase adapter error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 import { db } from "../../../firebase.config";
+import * as firestoreFunctions from "firebase/firestore";
 
 export default NextAuth({
   providers: [
@@ -10,5 +11,8 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_WEB_SECRET,
     }),
   ],
-  adapter: FirebaseAdapter(db),
+  adapter: FirebaseAdapter({
+    db,
+    ...firestoreFunctions,
+  }),
 });
