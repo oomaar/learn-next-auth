c4d6be353e9d8e0ce2390a466b1468fa0944b90a
oomaar <omaar_5@hotmail.com>
2022-05-14 17:27:01 +0200

    confif ext-aut with google provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import { db } from "../../../firebase.config";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_WEB_SECRET,
+    }),
+  ],
+  adapter: FirebaseAdapter(db),
+});
