d3eeb32d4df1aa9457837c48c9d42009428bf452
oomaar <omaar_5@hotmail.com>
2022-05-14 19:00:07 +0200

    handle the GetUserByAccountError

---
+++ Notes.js
@@ -0,0 +1,8 @@
+// To solve GetUserByAccountError
+
+// Go to Nodemodules
+// Go to @nextauth module
+// Go to dist -> index.js
+// Search for toDate
+// in the if condition add a ? before the toDate
+// stop and restart the server the error will go away
