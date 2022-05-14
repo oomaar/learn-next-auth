e49888c731e3a45bdcd73e000bff6397ae806cec
oomaar <omaar_5@hotmail.com>
2022-05-14 15:24:55 +0200

    add login button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>index</div>;
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-slate-50">
+      <div className="flex w-64 h-auto px-4 py-2 border rounded-md border-gray-300 items-center justify-center">
+        Login
+      </div>
+    </div>
+  );
 };
 
 export default Home;
