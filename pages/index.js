8a5a2b1652095dad3cae13840413ab3cf4fd6a2e
oomaar <omaar_5@hotmail.com>
2022-05-14 15:27:24 +0200

    customize the button: [Sign in with Google]

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { FcGoogle } from "react-icons/fc";
+
 const Home = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-slate-50">
-      <div className="flex w-64 h-auto px-4 py-2 border rounded-md border-gray-300 items-center justify-center">
-        Login
+      <div className="flex cursor-pointer w-64 h-auto px-4 py-2 border rounded-md border-gray-300 items-center justify-center">
+        <FcGoogle fontSize={30} className="mr-4" />
+        <span>Sign in with Google</span>
       </div>
     </div>
   );
